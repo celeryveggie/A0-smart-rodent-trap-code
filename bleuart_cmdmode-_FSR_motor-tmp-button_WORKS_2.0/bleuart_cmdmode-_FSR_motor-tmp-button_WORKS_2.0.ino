293c1cfd78c4499a901464b01807f0d098b37c16
celeryveggie <95715681+celeryveggie@users.noreply.github.com>
2021-12-08 10:09:13 -0500

    Add files via upload

---
+++ bleuart_cmdmode-_FSR_motor-tmp-button_WORKS_2.0/bleuart_cmdmode-_FSR_motor-tmp-button_WORKS_2.0.ino
@@ -0,0 +1,239 @@
+/* FSR testing sketch. 
+ 
+Connect one end of FSR to power, the other end to Analog 0.
+Then connect one end of a 10K resistor from Analog 0 to ground 
+ 
+For more information see www.ladyada.net/learn/sensors/fsr.html */
+/*********************************************************************
+ This is an example for our nRF51822 based Bluefruit LE modules
+
+ Pick one up today in the adafruit shop!
+
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+#include <Arduino.h>
+#include <SPI.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+#if SOFTWARE_SERIAL_AVAILABLE
+  #include <SoftwareSerial.h>
+#endif
+
+#define FACTORYRESET_ENABLE         1
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
+
+//The motor tmp36 and button code includes  
+
+#include <Servo.h>
+#define tempPin  A0
+Servo Myservo;
+int pos=180;
+int n=0; // the loop for avg
+int N=15;  //the amount of times it mesures until it sums 
+float accumsum =0;  //temps summed
+float avgtem= 70.0; // the avg of the sums 
+
+// Create the bluefruit object, either software serial...uncomment these lines
+
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+ 
+int fsrPin = 1;     // the FSR and 10K pulldown are connected to a1
+int fsrReading;     // the analog reading from the FSR resistor divider
+int fsrVoltage;     // the analog reading converted to voltage
+unsigned long fsrResistance;  // The voltage converted to resistance, can be very big so make "long"
+unsigned long fsrConductance; 
+long fsrForce;       // Finally, the resistance converted to force
+ 
+void setup(void) {
+  
+Serial.begin(115200);   //this is the setup for the mot-tmp-but                                            
+  Myservo.attach(3);   // where the servo motor is attached 
+  pinMode(2,INPUT);   //is where the putton is attached 
+
+  
+  Serial.begin(115200);   // We'll send debugging information via the Serial monitor
+while (!Serial);  // required for Flora & Micro
+  delay(500);
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit Command Mode Example"));
+  Serial.println(F("---------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println(F("Then Enter characters to send to Bluefruit"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("******************************"));
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+    Serial.println(F("******************************"));
+  }
+  
+}
+ 
+void loop(void) {
+// the loop code that works for tmp-mot-button
+  float value = analogRead(tempPin);
+  float tempr = (value * 500) / 1024; 
+  accumsum= accumsum + tempr;
+  n=n+1;
+  if (n==N){
+    avgtem= accumsum/N;
+    n=0;
+    accumsum =0;
+  }//end of n=n
+  Serial.print(avgtem);
+  Serial.print("Temperature: ");
+  Serial.print(tempr);
+  Serial.print(" F");
+  Serial.println();   // Prints in serial monitor 
+  delay(1000);
+
+if (avgtem < 65 || avgtem > 75)
+{    
+   delay(100);
+   Myservo.write(90);
+   delay(2000);
+ } 
+ else if(digitalRead(2)==LOW)
+ {
+  delay(10000);
+  Myservo.write(90);
+  delay(5000);
+ }
+ else
+ {
+  delay(100);
+  Myservo.write(pos);
+  delay(2000);
+ }
+
+//end of tmp-but-mot code
+
+  
+    // Check for user input
+    char inputs[BUFSIZE+1];
+  
+  fsrReading = analogRead(fsrPin);  
+  Serial.print("Analog reading = ");
+  Serial.println(fsrReading);
+ 
+  // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
+  fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
+  Serial.print("Voltage reading in mV = ");
+  Serial.println(fsrVoltage);  
+ 
+  if (fsrVoltage == 0) {
+    Serial.println("No pressure");  
+  } else {
+    // The voltage = Vcc * R / (R + FSR) where R = 10K and Vcc = 5V
+    // so FSR = ((Vcc - V) * R) / V        yay math!
+    fsrResistance = 5000 - fsrVoltage;     // fsrVoltage is in millivolts so 5V = 5000mV
+    fsrResistance *= 10000;                // 10K resistor
+    fsrResistance /= fsrVoltage;
+    Serial.print("FSR resistance in ohms = ");
+    Serial.println(fsrResistance);
+ 
+  }
+  Serial.println("--------------------");
+  delay(1000);
+  if (fsrVoltage > 0){
+
+    
+    ble.print("AT+BLEUARTTX=");
+    ble.println("Rodent Trapped in #1");
+
+    // check response stastus
+    if (! ble.waitForOK() ) {
+      Serial.println(F("Failed to send?"));
+    }
+  }
+ // Check for incoming characters from Bluefruit
+  ble.println("AT+BLEUARTRX");
+  ble.readline();
+  if (strcmp(ble.buffer, "OK") == 0) {
+    // no data
+    return;
+  }
+  // Some data was found, its in the buffer
+  Serial.print(F("[Recv] ")); Serial.println(ble.buffer);
+  ble.waitForOK();
+  
+}
+bool getUserInput(char buffer[], uint8_t maxSize)
+{
+  // timeout in 100 milliseconds
+  TimeoutTimer timeout(100);
+
+  memset(buffer, 0, maxSize);
+  while( (!Serial.available()) && !timeout.expired() ) { delay(1); }
+
+  if ( timeout.expired() ) return false;
+
+  delay(2);
+  uint8_t count=0;
+  do
+  {
+    count += Serial.readBytes(buffer+count, maxSize);
+    delay(2);
+  } while( (count < maxSize) && (Serial.available()) );
+
+  return true;
+}
